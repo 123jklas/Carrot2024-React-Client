ae450b234f0a48f4147623d8b9a8b82ee1bcc805
Jin Jun Oh <jinjunoh2@gmail.com>
2024-11-15 01:53:17 -0600

    cleanup theme util

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,9 +1,9 @@
 import {createTheme} from '@mui/material/styles'
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Rubik, Arial, sans-serif',
-  },
-})
-
-export default theme
+export default function theme() => {
+    createTheme({
+        typography: {
+            fontFamily: 'Rubik, Arial, sans-serif',
+        },
+    })
+}
