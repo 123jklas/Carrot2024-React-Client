ea60a628ad35ff99e1b05b994e9ae6c205a5fdd4
Makyra Kim <makyra20412@gmail.com>
2025-01-29 18:24:29 -0600

    post view frontend 1_25

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import Home from './pages/Home/Home'
 import Profile from './pages/Profile/Profile'
+import Post from './pages/Post/Post'
 import './assets/styles/App.css'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/post" element={<Post />} />
         </Routes>
       </div>
     </Router>
