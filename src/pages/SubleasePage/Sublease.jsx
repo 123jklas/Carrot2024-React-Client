1d5da5dc1a46850e27d79d336fa4f1fe115f3b09
123jklas <minjoy0321@gmail.com>
2025-04-14 12:30:01 -0500

    Sublease page

---
+++ src/pages/SubleasePage/Sublease.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import '../../assets/styles/Sublease.css'
+const SubleasePage = () => {
+    return(   
+        <div className="Sublease-container"> 
+            <Navbar/>
+            <h1> Sublease Page </h1>
+        </div>
+    )
+}
+export default SubleasePage
